0ad35085d8adc15513e8301f46baef059ee84564
faraz <farazsoomro602@gmail.com>
2022-03-27 21:28:12 +0500

    modifications

--- scraper.py
+++ scraper.py
@@ -9,9 +9,8 @@ topics_url = 'https://github.com/topics'
 response = req.get(topics_url)
 page_contents = response.text
 # print('status', response.status_code)
-
-#with open('web_page.html', 'w') as w:
-#    w.write(response.text)
+with open('web_page.html', 'w') as w:
+    w.write(response.text)
 
 doc = bs(page_contents, 'html.parser')
 
@@ -45,31 +44,36 @@ df.to_csv('github_topics.csv')
 ####################################### Scrapping each topic page #######################################
 
 # topic_page_url = 'https://github.com/topics/algorithm'
+
+path = '/home/farazsoomro/Scraper'
+os.chdir(path)
+os.makedirs('Topic Repos', exist_ok=True)
 for link in links:
     topic_page_url = link
     topic_page_response = req.get(topic_page_url)
     # print('status', topic_page_response.status_code)
 
-    doc = bs(topic_page_response.text, 'html.parser')
+    topic_doc = bs(topic_page_response.text, 'html.parser')
 
-    topic = doc.find_all('h1', {'class': 'h1'})
+    topic_name = topic_doc.find_all('h1', {'class': 'h1'})
+    # print(topic_name)
+    
+    users = topic_doc.find_all('a', {'data-ga-click': 'Explore, go to repository owner, location:explore feed'})
 
-    users = doc.find_all('a', {'data-ga-click': 'Explore, go to repository owner, location:explore feed'})
-
-    name = users[0]
-    print(name)
+    # name = users[0]
+    # print(name)
 
     username = []
     for user in users:
         username.append(user.text.strip())
-    print('username', len(username))
+    # print('username', len(username))
 
     user_profile = []
     for profile in users:
         user_profile.append(base_url + profile.get('href'))
     # print('user_profile', len(user_profile))
 
-    repository = doc.find_all('a', {'class': 'text-bold wb-break-word'})
+    repository = topic_doc.find_all('a', {'class': 'text-bold wb-break-word'})
     # print(len(repository))
 
     user_git_repo = []
@@ -77,7 +81,7 @@ for link in links:
         user_git_repo.append(base_url + repo.get('href'))
     # print('user_git_repo',len(user_git_repo))
 
-    repo_stars = doc.find_all('span', {'id': 'repo-stars-counter-star'})
+    repo_stars = topic_doc.find_all('span', {'id': 'repo-stars-counter-star'})
     user_repo_stars = []
     for stars in repo_stars:
         user_repo_stars.append(stars.text + ' Stars')
@@ -88,7 +92,8 @@ for link in links:
     }
 
     topic_df = pd.DataFrame(topic_repos_dict, index=None)
-    print(topic_df)
-
+    # print(topic_df)
+    # fname = topic.text.strip()
     # path = os.makedirs('data', exist_ok=True)
-    # topic_df.to_csv('path/{}.csv'.format('topic_repo'))
+    for fname in topic_name:
+        topic_df.to_csv('Topic Repos/{}.csv'.format(fname.text.strip()))
